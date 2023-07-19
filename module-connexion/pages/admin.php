b67dd5a2b3f66de8baa6cbac80b761319fc7b930
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-07-19 15:43:13 +0200

    first commit

--- module-connexion/pages/admin.php
+++ module-connexion/pages/admin.php
@@ -62,6 +62,7 @@ session_start();
         $password = "root";
         $dbname = "moduleconnexion";
 
+
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
